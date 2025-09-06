19840d5d5f780a81d93387f34a43ab8fca7bdfba
Cristina Vera <99357695+CVG42@users.noreply.github.com>
2025-09-06 10:48:44 -0600

    [2] Player base movement (#9)
    * Ground layer addition
    
    * InputManager script creation
    
    * PlayerController and PlayerInputVariables scripts creation
    
    Included the BasePlayer SO creation.
    
    * InputManager prefab creation
    
    * Test scene change

---
+++ virus-project/Assets/_Project/Scripts/Player/PlayerInputVariables.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Virus
+{
+    [CreateAssetMenu(fileName = "PlayerVariables", menuName = "Player Input/Variables")]
+    public class PlayerInputVariables : ScriptableObject
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _jumpForce;
+        [SerializeField] private float _fallMultiplier;
+        [SerializeField] private float _ascendMultiplier;
+        [SerializeField] private LayerMask _groundLayer;
+
+        public float MoveSpeed => _moveSpeed;
+        public float JumpForce => _jumpForce;
+        public float FallMultiplier => _fallMultiplier;
+        public float AscendMultiplier => _ascendMultiplier;
+        public LayerMask GroundLayer => _groundLayer;
+    }
+}
