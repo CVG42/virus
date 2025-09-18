68b054c4ff0cda6fdb5cdeeb82a17e4d316a2a58
CVG42 <zap653@amerike.edu.mx>
2025-09-18 14:16:14 -0600

    Typing events scripts

---
+++ virus-project/Assets/_Project/Scripts/Typing/AntivirusEvent.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Virus
+{
+    public class AntivirusEvent : MonoBehaviour
+    {
+        [SerializeField] private TextData _textData;
+
+        private bool _isActivated = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_isActivated) return;
+            if (!other.CompareTag("Player")) return;
+
+            GameStateManager.Source.ChangeState(GameState.OnAntivirusEvent);
+
+            _isActivated = true;
+            TypingManager.Source.StartTypingEvent(_textData);
+            TypingManager.Source.OnTypingCompleted += HandleEventCompleted;
+        }
+
+        private void HandleEventCompleted()
+        {
+            TypingManager.Source.DeactivateUI();
+            GameStateManager.Source.ChangeState(GameState.OnPlay);
+        }
+
+        private void OnDisable()
+        {
+            TypingManager.Source.OnTypingCompleted -= HandleEventCompleted;
+        }
+    }
+}
---
+++ virus-project/Assets/_Project/Scripts/Typing/GameTypingEvent.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Virus
+{
+    public class GameTypingEvent : MonoBehaviour
+    {
+        [SerializeField] private List<TextData> _wordsToType;
+
+        private int _currentWordIndex = 0;
+        private bool _isActivated = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (_isActivated) return;
+            if (!other.CompareTag("Player")) return;
+
+            GameStateManager.Source.ChangeState(GameState.OnTyping);
+
+            if (_wordsToType.Count == 0) return;
+
+            _isActivated = true;
+            _currentWordIndex = 0;
+            StartNextWord();
+        }
+
+        private void StartNextWord()
+        {
+            if (_currentWordIndex >= _wordsToType.Count)
+            {
+                TypingManager.Source.DeactivateUI();
+                GameStateManager.Source.ChangeState(GameState.OnPlay);
+                return;
+            }
+
+            TextData currentWord = _wordsToType[_currentWordIndex];
+            TypingManager.Source.OnTypingCompleted += HandleWordCompleted;
+            TypingManager.Source.StartTypingEvent(currentWord);
+        }
+
+        private void HandleWordCompleted()
+        {
+            TypingManager.Source.OnTypingCompleted -= HandleWordCompleted;
+            _currentWordIndex++;
+            StartNextWord();
+        }
+
+        private void OnDisable()
+        {
+            TypingManager.Source.OnTypingCompleted -= HandleWordCompleted;
+        }
+    }
+}
